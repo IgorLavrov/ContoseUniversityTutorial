0984ffde02b80849304da2152d94ec4bd0ad7825
“Igor <igor.lavrov90@hotmail.co.uk>
2023-09-14 11:01:57 +0300

    Task6,7,8 completed

--- ContoseUniversityTutorial/Startup.cs
+++ ContoseUniversityTutorial/Startup.cs
@@ -25,8 +25,10 @@ namespace ContoseUniversityTutorial
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<SchoolContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddControllersWithViews();
         }
 
