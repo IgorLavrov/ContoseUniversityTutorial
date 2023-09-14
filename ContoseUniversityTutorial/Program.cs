2dacc14e880101495debb2c7af9cf40907fcacea
“Igor <igor.lavrov90@hotmail.co.uk>
2023-09-14 12:08:36 +0300

    Task11-Program.cs (Dispose the context when the Initialize method completes as shown in the following code:)

--- ContoseUniversityTutorial/Program.cs
+++ ContoseUniversityTutorial/Program.cs
@@ -17,8 +17,21 @@ namespace ContoseUniversityTutorial
         {
             var host = CreateHostBuilder(args).Build();
 
-            CreateDbIfNotExists(host);
-
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            mm
             host.Run();
         }
 
