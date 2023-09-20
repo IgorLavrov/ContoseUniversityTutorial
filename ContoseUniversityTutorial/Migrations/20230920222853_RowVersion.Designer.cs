5db7632dedfdebd95ebb5cd524f710591b831715
DESKTOP-NEQI063\Laptop <igor.lavrov90@hotmail.co.uk>
2023-09-21 01:49:05 +0300

    Task 39 committed Department  section created

--- ContoseUniversityTutorial/Migrations/20230920222853_RowVersion.Designer.cs
+++ ContoseUniversityTutorial/Migrations/20230920222853_RowVersion.Designer.cs
@@ -4,14 +4,16 @@ using ContoseUniversityTutorial.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContoseUniversityTutorial.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20230920222853_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ContoseUniversityTutorial/Models/Department.cs
+++ ContoseUniversityTutorial/Models/Department.cs
@@ -23,6 +23,8 @@ namespace ContoseUniversityTutorial.Models
 
         public int? InstructorID { get; set; }
 
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
